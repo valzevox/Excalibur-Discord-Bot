6d285224220b88522d2198b4be7c5bd192da0fc2
valzevox <valzevox@gmail.com>
2025-10-06 02:17:24 +0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,46 +1,31 @@
-package main
-
+// imports
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
+//starts http server
 func main() {
-	godotenv.Load()
-	BOT_TOKEN := os.Getenv("BOT_TOKEN")
-	sess, err := discordgo.New("Bot " + BOT_TOKEN)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		http.HandleFunc("/", getRoot)
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	// Handlers
-	sess.AddHandler(onGuildCreate)
-	sess.AddHandler(LeaveEveryServer)
+}
 
-	//Intents
-	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
-
-	//Authorization
-	err = sess.Open()
-
-	//Set Status
-	sess.UpdateStreamingStatus(0, "Excalibur / Blood Group", "https://www.twitch.tv/404")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sess.Close()
-
-	fmt.Println("The bot is online!\n\n[/] TOKEN: " + BOT_TOKEN + "\n[/] LINK: https://discord.com/api/oauth2/authorize?client_id=" + sess.State.User.ID + "&permissions=8&scope=bot")
-
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Excalibur is at render.com now.. 🚀\n")
 }
